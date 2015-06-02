b0c286df412ecbbeb6f73cc86006dcb25ac41eae
Michael Shaffer <michael.shaffer@ucdenver.edu>
2015-06-02 17:30:28 -0600

    added init.py and setup.py, changed scripts to correl_nets to create correl_nets package

---
+++ setup.py
@@ -0,0 +1,10 @@
+__author__ = 'shafferm'
+
+from setuptools import setup, find_packages
+
+setup(
+    name="correl_nets",
+    packages=find_packages(),
+    version="0.1",
+    packages=find_packages()
+)

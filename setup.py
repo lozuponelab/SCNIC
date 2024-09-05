e64ba774c34ddfad22fdc542f10ab2cdfa44b9ed
John Sterrett <sterrettJD@protonmail.com>
2024-09-05 16:59:18 -0600

    bump version and deps

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 __author__ = 'lozuponelab'
-__version__ = '0.6.3'
+__version__ = '0.6.4'
 
 setup(
       name="SCNIC",
       version=__version__,
       setup_requires=['pytest-runner'],
       test_require=['pytest'],
-      install_requires=["numpy", "scipy", "networkx>=2", "biom-format", "pandas>=1", "scikit-bio", "statsmodels",
+      install_requires=["numpy", "scipy>=1.9.0,<=1.10.1", "networkx>=2", "biom-format", "pandas>=1", "scikit-bio", "statsmodels", "tqdm", "seaborn",
                         "h5py"],
       scripts=['scripts/SCNIC_analysis.py', 'scripts/module_enrichment.py'],
       packages=find_packages(),
